ab6691ee38b88686195802363fbd03682caa4a0f
Jamil Seddiqi <jamilsediq@gmail.com>
2020-03-03 02:57:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,7 @@
   end 
   
   def add_student(student, level)
-    # this sort of means #=> a || a = b ... or #=> x || x = y
-    # it's freak'n weird
+
     roster[level] ||= []
     roster[level] << student
   end 
@@ -21,15 +20,6 @@
     end 
   end 
   
-# This works, but it isn't passing learn.co's tests cause of 
-# how it displays syntactically...but the same information is there
-#  def sort 
-#    roster.sort_by do |x, y| 
-#      x 
-#    end 
-#  end 
-
-# Here's learn.co's solution:
 def sort 
   nu_hash = {}
   roster.each do |x, y| 
@@ -40,18 +30,16 @@ end
   
 end 
 
-
-
-# school = School.new("Bayside High School")
-# school.roster
-# school.add_student("Zach Morris", 9)
-# school.add_student("Moshe Zeus", 9)
-# school.add_student("Onkelos Octavius", 9)
-# school.roster
-# school.add_student("Sugar Zaza", 5)
-# school.roster
-# school.add_student("Theo Madus", 5)
-# school.add_student("Abbah Zabbah", 2)
-# school.roster
-# school.grade(9)
-# school.sort
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.add_student("Moshe Zeus", 9)
+school.add_student("Onkelos Octavius", 9)
+school.roster
+school.add_student("Sugar Zaza", 5)
+school.roster
+school.add_student("Theo Madus", 5)
+school.add_student("Abbah Zabbah", 2)
+school.roster
+school.grade(9)
+school.sort
