a37eac1cd4229b639fa28560b0fbd0dae5ecced0
Jamil Seddiqi <jamilsediq@gmail.com>
2020-03-03 02:59:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,4 @@
-# code here!class School
+class School
   attr_accessor :name, :roster
   
   def initialize(name)
@@ -7,7 +7,8 @@
   end 
   
   def add_student(student, level)
-
+    # this sort of means #=> a || a = b ... or #=> x || x = y
+    # it's freak'n weird
     roster[level] ||= []
     roster[level] << student
   end 
@@ -20,6 +21,15 @@
     end 
   end 
   
+# This works, but it isn't passing learn.co's tests cause of 
+# how it displays syntactically...but the same information is there
+#  def sort 
+#    roster.sort_by do |x, y| 
+#      x 
+#    end 
+#  end 
+
+# Here's learn.co's solution:
 def sort 
   nu_hash = {}
   roster.each do |x, y| 
@@ -29,6 +39,8 @@ def sort
 end 
   
 end 
+
+
 
 school = School.new("Bayside High School")
 school.roster
